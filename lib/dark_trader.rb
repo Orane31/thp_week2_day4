68f30b688e913ce7f2c624281c003ca86b1039ec
Orane31 <orane.chaper@gmail.com>
2020-07-16 21:06:00 +0200

    exo 2 terminé

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -6,51 +6,38 @@ require 'rspec'
 require 'rubocop'
 require 'open-uri'
 
-page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
-
-
-def acquire_page(page)
-    classpage = page.class 
-    if classpage = Nokogiri::HTML::Document 
-        return "The page is ready to be scrapped"
-    else
-        return "Problem occurred"
-    end
+def access_page
+    page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+    return page
 end
+      
 
-puts acquire_page(page)
-
-def get_names(page)
+def final_hash
+    page = access_page
     all_names = page.xpath('/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div')
     #puts all_names
+    all_prices = page.xpath('/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/a')
+    #puts all prices
 
     names_list = Array.new
         all_names.each do |name|
         names_list << name.text
         end
-    return names_list
-end
-
-names = get_names(page)
-
-def get_prices(page)
-    all_prices = page.xpath('/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/a')
-    #puts all prices
 
     prices_list = Array.new
         all_prices.each do |price|
         prices_list << price.text
-    end
-    return prices_list
-end
+        end
 
-prices = get_prices(page)
-
-def final_hash(names, prices)
     final_list = Hash.new 
-    names.zip(prices){|k,v|final_list[k] = v}
-    hola = final_list.each_slice(1).map(&:to_h)
-
+    names_list.zip(prices_list){|k,v|final_list[k] = v}
+    hello = final_list.each_slice(1).map(&:to_h)
+    return hello 
 end
 
-puts final_hash(names, prices)
+def perform 
+    access_page
+    final_hash
+end
+
+p perform
---
+++ lib/mairie_christmas.rb
@@ -0,0 +1,65 @@
+require 'rubygems'
+require 'nokogiri'
+require 'dotenv'
+require 'pry'
+require 'rspec'
+require 'rubocop'
+require 'open-uri'
+
+=begin
+Tu dois être capable d'obtenir un e-mail de mairie à partir de la page de cette dernière (pas en partant du listing complet des mairies). Fais d'abord une méthode get_townhall_email(townhall_url) qui récupère l’e-mail d'une mairie à partir de l'URL de mairies, par exemple celle de Avernes.
+Une fois que tu sais le faire pour une mairie, tu vas vouloir industrialiser et répéter ça sur tout l'annuaire du Val d'Oise. La prochaine étape est de coder une méthode get_townhall_urls qui récupère les URLs de chaque ville du Val d'Oise.
+=end
+
+avernes_url = 'http://annuaire-des-mairies.com/95/avernes.html'
+
+def get_townhall_email(townhall_url)
+    townhall_page = Nokogiri::HTML(URI.open(townhall_url))
+    email = townhall_page.xpath('/html/body/div[1]/main/section[2]/div/table/tbody/tr[4]/td[2]')
+    return email.text
+end
+
+#p get_townhall_email(avernes_url)
+
+
+def get_townhall_names
+    page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    towns_names = page.xpath('//a[contains(@href, "./")]')
+    all_names = Array.new
+    towns_names.each do |names|
+        all_names << names.text
+    end
+    return all_names
+end
+
+#p get_townhall_names
+
+def get_townhall_urls
+    page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    towns_names = page.xpath('//a[contains(@href, "./")]')
+    towns_links = Array.new
+    towns_names.each do |link|
+        towns_links << "http://annuaire-des-mairies.com/" +(link[:href])[2..-1]
+    end
+    return towns_links
+end
+
+#p get_townhall_urls
+
+
+def perform
+    
+    mails_list = Array.new
+    get_townhall_urls.each do |url|
+        mails_list << get_townhall_email(url)
+    end
+
+    together = Hash.new
+    get_townhall_names.zip(mails_list){|k,v|together[k] = v}
+    final_list = together.each_slice(1).map(&:to_h)
+    p final_list
+    
+end
+
+perform
+
